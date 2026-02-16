c5801269f5bb905650f0d6363f1fdbbbf0932e38
Itamar Junior <itamar.junior@irriga.net>
2026-02-16 16:26:38 -0300

    feat: implementar as rotas

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\LineController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('clients', ClientController::class);
+Route::apiResource('cars', CarController::class);
+Route::apiResource('rentals', RentalController::class);
+Route::apiResource('brands', BrandController::class);
+Route::apiResource('lines', LineController::class);
